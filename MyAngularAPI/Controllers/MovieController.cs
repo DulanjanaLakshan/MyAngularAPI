5271d6f73d489cf3b84a89d18f6f8d2c7125c136
dulanjanalakshan <dulanjana20013@gmail.com>
2022-11-24 21:00:21 +0530

    Add project files.

---
+++ MyAngularAPI/Controllers/MovieController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyAngularAPI.models;
+
+namespace MyAngularAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        [HttpGet]
+        public List<Movie> GetAllMovies()
+        {
+            List<Movie> movies = new List<Movie>();
+            movies.Add(new Movie { Id = "M001", Title = "Spider Man", Description = "Sample Descriptio 01" });
+            movies.Add(new Movie { Id = "M002", Title = "Iorn Man", Description = "Sample Descriptio 02" });
+            movies.Add(new Movie { Id = "M003", Title = "Mr:Been", Description = "Sample Descriptio 03" });
+            return movies;
+        }
+    }
+}
